792f0f00ca2134c9017fa41cc601e13f57944aff
Julian Nicholls <juliannicholls29@gmail.com>
2018-11-10 20:49:24 +0000

    Split out turtle class to separate file.

--- logo.js
+++ logo.js
@@ -1,53 +1,3 @@
-class Turtle {
-  constructor(ctx) {
-    this.ctx = ctx;
-    this.angle = -Math.PI / 2;
-    this.x = 250;
-    this.y = 250;
-    this._penDown = true;
-
-    ctx.strokeStyle = '#fff';
-  }
-
-  forward(delta) {
-    const { x: curX, y: curY, angle, ctx, _penDown } = this;
-
-    this.x += Math.cos(angle) * delta;
-    this.y += Math.sin(angle) * delta;
-
-    if (_penDown) {
-      ctx.beginPath();
-      ctx.moveTo(curX, curY);
-      ctx.lineTo(this.x, this.y);
-      ctx.stroke();
-    }
-  }
-
-  right(angle) {
-    let newAngle = this.angle + (angle * Math.PI) / 180;
-
-    if (newAngle > 2 * Math.PI) newAngle -= 2 * Math.PI;
-
-    this.angle = newAngle;
-  }
-
-  left(angle) {
-    let newAngle = this.angle - (angle * Math.PI) / 180;
-
-    if (newAngle < 0) newAngle += 2 * Math.PI;
-
-    this.angle = newAngle;
-  }
-
-  penUp() {
-    this._penDown = false;
-  }
-
-  penDown() {
-    this._penDown = true;
-  }
-}
-
 const canvas = document.getElementById('logo-output');
 const ctx = canvas.getContext('2d');
 
@@ -56,36 +6,15 @@ ctx.fillRect(0, 0, 600, 600);
 
 const turtle = new Turtle(ctx);
 
-turtle.forward(100);
-turtle.right(60);
-
-turtle.forward(100);
-turtle.right(60);
-
 turtle.penUp();
 
-turtle.forward(100);
-turtle.right(60);
+turtle.left(135);
+turtle.forward(200);
+turtle.right(135);
 
 turtle.penDown();
 
-turtle.forward(100);
-turtle.right(60);
-
-turtle.penUp();
-
-turtle.forward(100);
-turtle.right(60);
-
-turtle.penDown();
-
-turtle.forward(100);
-
-//   ctx.strokeStyle = '#fff';
-
-// ctx.beginPath();
-// ctx.moveTo(100, 100);
-// ctx.lineTo(200, 100);
-// ctx.lineTo(150, 187);
-// ctx.lineTo(100, 100);
-// ctx.stroke();
+for (let i = 0; i < 90; ++i) {
+  turtle.forward(300);
+  turtle.right(92);
+}
